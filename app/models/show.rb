692b7d35b20bc230fda1433c2b257b758f976a43
NickSpangler <Spangleyman@mac.com>
2020-10-13 15:32:30 -0400

    Final association wiring

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,5 @@
 class Show < ApplicationRecord
-    belongs_to :creator, :class_name => "User"
+    belongs_to :creator, class_name: "User"
     has_many :contributions
+    has_many :contributors, through: :contributions, source: :contributor
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :created_shows, :class_name => "Show", :foreign_key => :creator_id
+    has_many :created_shows, class_name: "Show", foreign_key: :creator_id
     has_many :contributions
-    has_many :contributed_shows, :through => :contributions, :source => :show
+    has_many :contributed_shows, through: :contributions, source: :show
 
 end
