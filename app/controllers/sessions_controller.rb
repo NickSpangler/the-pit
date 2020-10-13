8549925b76cc1a580d0f2a4d574769c3dceea0be
NickSpangler <Spangleyman@mac.com>
2020-10-13 16:24:42 -0400

    build omniauth login sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,20 @@ class SessionsController < ApplicationController
     end
   
     def create
-      user = User.find_by(name: params[:user][:name])
-      user = user.try(:authenticate, params[:user][:password])
-      return redirect_to(controller: 'sessions', action: 'new') unless user
-      session[:user_id] = user.id
-      @user = user
-      redirect_to user_dashboard_path(@user)
+      if auth_hash = request.env["omniauth.auth"]
+        raise auth_hash.inspect
+      else
+        user = User.find_by(name: params[:user][:name])
+        user = user.try(:authenticate, params[:user][:password])
+        return redirect_to(controller: 'sessions', action: 'new') unless user
+        session[:user_id] = user.id
+        @user = user
+        redirect_to user_dashboard_path(@user)
+      end
     end
   
     def destroy
       session.delete :user_id
-      redirect_to '/'
+       redirect_to '/'
     end
 end
