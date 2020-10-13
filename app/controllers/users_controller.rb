8bf7f9af7cd2c831a648a6ca05163b6ee83f8b86
NickSpangler <Spangleyman@mac.com>
2020-10-13 10:30:04 -0400

    Add users#dashboard action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.create(user_params)
+        return redirect_to controller: 'users', action: 'new' unless @user.save
+        session[:user_id] = @user.id
+        redirect_to controller: 'welcome', action: 'home'
+    end
+
+    def dashboard
+        @user = User.find_by(id: session[:user_id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
