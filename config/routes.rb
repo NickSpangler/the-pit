afbdb7ee4e4c0034c2c8a5183e5d73f214b6a3ed
NickSpangler <Spangleyman@mac.com>
2020-10-13 10:45:43 -0400

    Add users/:id/dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'static#welcome'
   get 'static/welcome'
   resources :users
+  get 'users/:id/dashboard', to: 'users#dashboard', as: 'user_dashboard'
 end
