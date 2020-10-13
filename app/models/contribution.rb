f6658244896ee4815b468f2e8fb6b6f0a25b1925
NickSpangler <Spangleyman@mac.com>
2020-10-13 15:19:11 -0400

    Finish wiring all associations

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,4 @@
 class Contribution < ApplicationRecord
-    belongs_to :user
+    belongs_to :contributor, :class_name => "User", :foreign_key => :user_id
     belongs_to :show
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,4 @@
 class Show < ApplicationRecord
     belongs_to :creator, :class_name => "User"
+    has_many :contributions
 end
