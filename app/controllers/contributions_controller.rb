84960954169a2b0576e8f0a4b12ee70c4cadfdad
NickSpangler <Spangleyman@mac.com>
2020-10-14 14:37:00 -0400

    build contribution_params

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,24 @@
 class ContributionsController < ApplicationController
+
+    def new
+        @contribution = Contribution.new
+    end
+
+    def create
+
+    end
+
+    def show
+        set_contribution
+    end
+
+    private
+
+    def contribution_params
+        params.require(:contribution).permit(:show_id, :user_id, :title, :content, :note_from_creator, :accepted, :pending_id)
+    end
+
+    def set_contribution
+        @contribution = Contribution.find_by(id: params[:id])
+    end
 end
