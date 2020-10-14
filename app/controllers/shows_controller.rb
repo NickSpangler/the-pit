ed957b319ea618a7fd305f0347ba4b5aac9246a9
NickSpangler <Spangleyman@mac.com>
2020-10-14 11:54:38 -0400

    add shows#show view

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,15 +1,24 @@
 class ShowsController < ApplicationController
     def new
-        @show = current_user.created_shows.build
+        @show = Show.new
     end
 
     def create
-        raise params.inspect
+        @show = current_user.created_shows.create(show_params)
+        redirect_to user_show_path(current_user, @show)
+    end
+
+    def show
+        set_show
     end
 
     private
 
     def show_params
         params.require(:show).permit(:title, :synopsis, :character_list, :creator_id)
+    end
+
+    def set_show
+        @show = Show.find_by(id: params[:id])
     end
 end
