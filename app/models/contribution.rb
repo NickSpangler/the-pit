4280c217904f2f430005e05f95d6f5fcc3391ab1
NickSpangler <Spangleyman@mac.com>
2020-10-15 09:53:40 -0400

    add :audio_file to Contribution model

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,4 +2,5 @@ class Contribution < ApplicationRecord
     belongs_to :contributor, class_name: "User", foreign_key: :user_id
     belongs_to :show
     has_one_attached :image
+    has_one_attached :audio_file
 end
