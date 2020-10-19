e2319fc4d5fa984bd9ebd0167f6c6e8b42f2a1e4
NickSpangler <Spangleyman@mac.com>
2020-10-19 17:40:46 -0400

    Add root path redirect if logged in user

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,10 @@
 class StaticController < ApplicationController
   skip_before_action :verified_user
+
   def welcome
+    if current_user
+      redirect_to user_dashboard_path(current_user)
+    end
   end
+
 end
